ebe8c5281bcf3508e859bf2b6e8eae995781b38d
Juliana Grutzmann <jgrutzmann@hubspot.com>
2023-08-25 11:48:52 -0400

    add tests for other repo info

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -1,20 +1,13 @@
-import { screen, render, act } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import RepositoriesListItem from './RepositoriesListItem';
 import { MemoryRouter } from 'react-router';
-
-// jest.mock('../tree/FileIcon', () => {
-//   //contents of FileIcon.js
-//   return () => {
-//     return 'File Icon Component'
-//   }
-// });
 
 function renderComponent() {
   const repository = {
     full_name: 'facebook/react',
     language: 'Javascript',
     description: 'A JS library',
-    owner: 'facebook',
+    owner: { login: 'facebook' },
     name: 'react',
     html_url: 'https://github.com/facebook/react'
   }
@@ -22,17 +15,37 @@ function renderComponent() {
   render(<MemoryRouter>
       <RepositoriesListItem repository={repository}/>
     </MemoryRouter>);
+
+    return { repository };
 }
 
 test('shows a link to the github homepage for this repository', async () => {
-  renderComponent();
-
-  await act(async () => {
-    await pause();
-  })
+  const { repository } = renderComponent();
 
   // solve the Act warning by finding the img element that is rendering async
-  // await screen.findByRole('img', { name: 'Javascript'});
+  await screen.findByRole('img', { name: 'Javascript'});
+
+  const link = screen.getByRole('link', { name: /github repository/});
+  expect(link).toHaveAttribute('href', repository.html_url);
 });
 
-const pause = () => new Promise(resolve => setTimeout(resolve, 100));
+test('shows a file icon with the appropriate icon', async () => {
+  renderComponent();
+
+  const icon = await screen.findByRole('img', { name: 'Javascript'});
+  expect(icon).toHaveClass('js-icon');
+});
+
+test('shows a link to the code editor page', async() => {
+  const { repository } = renderComponent();
+
+  await screen.findByRole('img', { name: 'Javascript'});
+
+  const link = await screen.findByRole('link', {
+    name: new RegExp(repository.owner.login)
+  })
+
+  expect(link).toHaveAttribute('href', `/repositories/${repository.full_name}`);
+})
+
+
