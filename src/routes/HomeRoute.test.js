8374aa5af5dba5d2ae444569aa1ae6a93e590524
Juliana Grutzmann <jgrutzmann@hubspot.com>
2023-08-25 16:43:48 -0400

    mocking data fetching with msw

---
+++ src/routes/HomeRoute.test.js
@@ -0,0 +1,51 @@
+import { screen, render } from '@testing-library/react';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+import { MemoryRouter } from 'react-router';
+import HomeRoute from './HomeRoute';
+
+const handlers = [
+  rest.get('/api/repositories', (req, res, ctx) => {
+    const language = req.url.searchParams.get('q').split('language:')[1];
+
+    return res(ctx.json({items: [
+      { id: 1, full_name: `${language}_one` },
+      { id: 2, full_name: `${language}_two`}
+    ]}))
+  })
+]
+
+const server = setupServer(...handlers);
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
+
+test('renders two links for each language', async () => {
+  render(
+    <MemoryRouter>
+      <HomeRoute />
+    </MemoryRouter>
+    );
+
+  screen.debug();
+  //loop over each language and make sure we see two links for each
+  const languages = ['javascript', 'typescript', 'rust', 'go', 'java', 'python'];
+
+  for (let language of languages) {
+    const links = await screen.findAllByRole('link', { name: new RegExp(`${language}_`)});
+
+    expect(links).toHaveLength(2);
+    expect(links[0]).toHaveAttribute('href', `/repositories/${language}_one`);
+    expect(links[1]).toHaveAttribute('href', `/repositories/${language}_two`);
+  }
+
+});
