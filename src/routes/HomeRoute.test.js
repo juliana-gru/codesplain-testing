7e67cbc8fc2e67d627cd21328f50281f4bf1e63f
Juliana Grutzmann <jgrutzmann@hubspot.com>
2023-08-26 11:10:25 -0400

    create mocked server to handle api requests

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -1,33 +1,24 @@
 import { screen, render } from '@testing-library/react';
-import { setupServer } from 'msw/node';
-import { rest } from 'msw';
 import { MemoryRouter } from 'react-router';
 import HomeRoute from './HomeRoute';
+import { createServer } from '../test/server';
 
-const handlers = [
-  rest.get('/api/repositories', (req, res, ctx) => {
-    const language = req.url.searchParams.get('q').split('language:')[1];
 
-    return res(ctx.json({items: [
-      { id: 1, full_name: `${language}_one` },
-      { id: 2, full_name: `${language}_two`}
-    ]}))
-  })
-]
+createServer([
+  {
+    path:'/api/repositories',
+    res: (req) => {
+      const language = req.url.searchParams.get('q').split('language:')[1];
 
-const server = setupServer(...handlers);
-
-beforeAll(() => {
-  server.listen();
-});
-
-afterEach(() => {
-  server.resetHandlers();
-});
-
-afterAll(() => {
-  server.close();
-});
+      return {
+        items: [
+          { id: 1, full_name: `${language}_one` },
+          { id: 2, full_name: `${language}_two`}
+        ]
+      }
+    }
+  }
+]);
 
 test('renders two links for each language', async () => {
   render(
@@ -36,7 +27,6 @@ test('renders two links for each language', async () => {
     </MemoryRouter>
     );
 
-  screen.debug();
   //loop over each language and make sure we see two links for each
   const languages = ['javascript', 'typescript', 'rust', 'go', 'java', 'python'];
 
@@ -44,7 +34,7 @@ test('renders two links for each language', async () => {
     const links = await screen.findAllByRole('link', { name: new RegExp(`${language}_`)});
 
     expect(links).toHaveLength(2);
-    expect(links[0]).toHaveAttribute('href', `/repositories/${language}_one`);
+    expect(links[0]).toHaveTextContent(`${language}_one`);
     expect(links[1]).toHaveAttribute('href', `/repositories/${language}_two`);
   }
 
